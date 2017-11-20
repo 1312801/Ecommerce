4031446ee80df1ffa21bf7831a91e0f60c42c322
1312801 <1312801@student.hcmus.edu.vn>
2017-11-21 01:26:07 +0700

    Login va signup co ban

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119151742) do
+ActiveRecord::Schema.define(version: 20171120111023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20171119151742) do
     t.string "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "country"
   end
 
 end
